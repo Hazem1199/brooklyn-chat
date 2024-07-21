e630be7ec759efbaac552ee74054b59fc7b6cb6a
Hazem Essam <133036561+Hazem1199@users.noreply.github.com>
2024-07-21 18:36:28 +0300

    21-7-2024

--- src/App.vue
+++ src/App.vue
@@ -92,7 +92,7 @@ export default {
             state.username = user;
             inputLogin.value = "";
           }
-          // console.log(user);
+          console.log(user);
         })
         .catch((error) => {
           console.log(error);
